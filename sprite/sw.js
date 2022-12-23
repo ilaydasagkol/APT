1df30af815beabab09035fed5e73408d00ec419e
ilaydasagkol <asiyeilayda.sagkol@stu.fsm.edu.tr>
2022-12-23 13:21:07 +0300

    sonCommit

---
+++ sprite/sw.js
@@ -0,0 +1,19 @@
+const CACHE ='https://ilaydasagkol.github.io/Hangman_Game/proje.html'
+const FILES = ['https://ilaydasagkol.github.io/Hangman_Game/proje.html']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+function cacheCB(e) { //cache first
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+  }
+  self.addEventListener('fetch', cacheCB)
